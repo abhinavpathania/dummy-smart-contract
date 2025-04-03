496863096100eab578c9fd6a6dd08fe42158bffe
Abhinav Pathania <112753370+abhinavpathania@users.noreply.github.com>
2025-04-03 10:07:40 +0530

    commit

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",  // Ganache RPC server
-      port: 7545,         // Ganache GUI default port
+      port: 7546,         // Ganache GUI default port
       network_id: "*",    // Any network
     },
   },
